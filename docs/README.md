3a1d03069c883f395228d5ec5be8de39c798800a
Alex Pawlowski <apawlows@vols.utk.edu>
2017-01-12 20:44:53 -0500

    Switched to new sheet with paired down information.
    Also, combined first and last names.

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+## Reading Google Spreadsheet as JSON for dynamic web content
+
+This repo covers a system to read in abstract submission information (once abstracts have been accepted for presentation) for Pipeline's Annual Women in STEM Symposium.
+
--- docs/read-json-gsheet.js
+++ docs/read-json-gsheet.js
@@ -2,7 +2,7 @@
  // ID of the Google Spreadsheet
  
  // ID of the Google Spreadsheet
- var spreadsheetID = "1gCubTVGtEpO02rSeuPHQiBezyeDhH2es9m7aZI0k_cI";
+ var spreadsheetID = "1B42bpHcFo7R7cfDvng8rSvyjWQxpiFTNqy46cCqmUZA";
  
  // Make sure it is public or set to Anyone with link can view 
  // apparently od6 stopped working in 2015: http://stackoverflow.com/questions/24531351/retrieve-google-spreadsheet-worksheet-json
@@ -20,7 +20,6 @@ $.getJSON(url, function(data) {
   oral_table += '<table cellpadding=10 cellspacing=0 border=1>';
   oral_table += '<tr>';
   oral_table += '<th>Name</th>';
-  oral_table += '<th>Last Name</th>';
   oral_table += '<th>Department</th>';
   oral_table += '<th>Abstract Title</th>';
   oral_table += '<th>Academic Status</th>';
@@ -30,7 +29,6 @@ $.getJSON(url, function(data) {
   poster_table += '<table cellpadding=10 cellspacing=0 border=1>';
   poster_table += '<tr>';
   poster_table += '<th>Name</th>';
-  poster_table += '<th>Last Name</th>';
   poster_table += '<th>Department</th>';
   poster_table += '<th>Abstract Title</th>';
   poster_table += '<th>Academic Status</th>';
@@ -45,8 +43,7 @@ $.getJSON(url, function(data) {
   for (var i = entry.length - 1; i >= 0; i-=1) {
       if (entry[i]['gsx$presentationtype']['$t'] == "Oral presentation"){
     oral_table += '<tr>';
-    oral_table += '<td>' + entry[i]['gsx$firstname']['$t'] + '</td>';
-    oral_table += '<td>' + entry[i]['gsx$lastname']['$t'] + '</td>';
+    oral_table += '<td>' + entry[i]['gsx$firstname']['$t'] + ' ' + entry[i]['gsx$lastname']['$t'] + '</td>';
     oral_table += '<td>' + entry[i]['gsx$department']['$t'] + '</td>';
     oral_table += '<td>' + entry[i]['gsx$abstracttitle']['$t'] + '</td>';
     oral_table += '<td>' + entry[i]['gsx$academicstatus']['$t'] + '</td>';
@@ -54,8 +51,7 @@ $.getJSON(url, function(data) {
       } else {
           console.log(entry[i]['gsx$presentationtype']['$t']);
     poster_table += '<tr>';
-    poster_table += '<td>' + entry[i]['gsx$firstname']['$t'] + '</td>';
-    poster_table += '<td>' + entry[i]['gsx$lastname']['$t'] + '</td>';
+    poster_table += '<td>' + entry[i]['gsx$firstname']['$t'] + ' ' + entry[i]['gsx$lastname']['$t'] + '</td>';
     poster_table += '<td>' + entry[i]['gsx$department']['$t'] + '</td>';
     poster_table += '<td>' + entry[i]['gsx$abstracttitle']['$t'] + '</td>';
     poster_table += '<td>' + entry[i]['gsx$academicstatus']['$t'] + '</td>';
